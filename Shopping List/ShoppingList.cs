2ccf10d306de03af212b53f30460cbe61b0a94d0
Durga Prasad Mishra <durg0001@edu.zealand.dk>
2023-03-28 10:44:51 +0200

    Add project files.

---
+++ Shopping List/ShoppingList.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Linq;
+
+namespace Shopping_List
+{
+    public class ShoppingList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+
+        public override string ToString()
+        {
+            return Id + Name + Price + Quantity;
+
+        }
+    }
+}
